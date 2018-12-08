7e63fb3262c940216e0030cca0db0b9b798b1f19
Patrick McCarty <patrickthesailorman@gmail.com>
2018-12-08 16:02:17 +0000

    update hotel2

---
+++ hotel2.js
@@ -0,0 +1,34 @@
+var hotel = {
+    name: "CareerDevs Mega Hotel",
+    rating: 5.0,
+    roomRate: 325.00,
+    roomNumbersAvailable: ["101", "102", "103", "104", "105", "106"],
+    roomNumbersBooked: [],
+    roomType: "Queen",
+
+    numberOfRoomsAvailable: function() {
+        // length of the number of rooms available, returned as an integer (1, 5, 20, etc)
+        return this.roomNumbersAvailable.length;
+    },
+
+    numberOfRoomsBooked: function() {
+        return this.roomNumbersBooked.length;
+    },
+
+    numberOfRooms: function() {
+        // avail rooms plus booked rooms
+        return this.roomNumbersAvailable.length + this.roomNumbersBooked.length;
+    }
+};
+
+// Everything below this line will get executed automatically
+
+document.getElementById("hotelName").innerText = hotel.name;
+
+//     roomNumbersAvailable: ["101", "102", "103", "104", "105", "106" ]
+
+var roomsListed = "<form> <select id='selectedRoom'>";
+            for (var i = 0; i < hotel.roomNumbersAvailable.length; i++) {
+
+                roomsListed += "<option value=" + hotel.roomNumbersAvailable[i] + ">" + hotel.roomNumbersAvailable[i] + "</option>";
+            }
